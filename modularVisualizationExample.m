d7b208bdb86729cab998836e7bf60a01c7110280
bpbratton <bratton@princeton.edu>
2016-10-25 07:54:57 -0500

    Improve "smoothness" of plotting with handles

--- modularVisualizationExample.m
+++ modularVisualizationExample.m
@@ -1,7 +1,7 @@
 %% initialize parameters
 b = 0.6;
 a = 0.08;
-dt = 0.1;
+dt = 0.01;
 
 %% calculate the dynamics for a system that starts at an arbitrary position
 % x0,y0
@@ -16,11 +16,20 @@ x0 = 2;
 y0 = 1;
 xNew = x0;
 yNew = y0;
+
+% save plotting handle to improve speed and smoothness
+pHand = plot(xNew,yNew,'r*','linewidth',2,'markersize',10);
+
+% set plotting limits to remain the same
+xlim([0,3]);
+ylim([0,3]);
+hold on;
+
 % loop over a few frames
-while counter < 10
+while counter < 100
     xNew = xNew + dt*xdot(xNew,yNew);
     yNew = yNew + dt*ydot(xNew,yNew);
-    plot(xNew,yNew,'rx');
+    set(pHand,'xData',xNew,'yData',yNew);
     figure(gcf);pause(0.1);
     counter = counter+1;
 end
