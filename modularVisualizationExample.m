569def74353235e48abd193eccf1e1227102b970
bpbratton <bratton@princeton.edu>
2016-10-25 08:02:26 -0500

    Use patchline from fex to add transparency to tail

--- modularVisualizationExample.m
+++ modularVisualizationExample.m
@@ -3,8 +3,8 @@ b = 0.6;
 a = 0.08;
 dt = 0.1;
 pauseTime = 0.00001;
-tailLength = 15;
-howCloseToFixedPoint = 0.00; % standard deviation in starting position
+tailLength = 50;
+howCloseToFixedPoint = 0.3; % standard deviation in starting position
 %% calculate the dynamics for a system that starts at an arbitrary position
 % x0,y0
 
@@ -15,8 +15,8 @@ xdot = @(x,y) y*x^2 - x + a*y;
 % proceed in one time step using Euler integration
 counter  = 1;
 
-xFix = 0.6;
-yFix = 0.6/(0.08+0.6^2);
+xFix = b;
+yFix = b/(b+a^2);
 
 x0 = xFix+randn(1)*howCloseToFixedPoint;
 y0 = yFix+randn(1)*howCloseToFixedPoint;
@@ -28,22 +28,28 @@ yNew = y0;
 tailX = repmat(x0,tailLength,1);
 tailY = repmat(y0,tailLength,1);
 
-
-
 % start with a blank graph
 clf;
+hold on;
 
 % save plotting handle to improve speed and smoothness
 pHand = plot(xNew,yNew,'r*','linewidth',2,'markersize',10);
-hold on;
-tailHand = plot(tailX,tailY,'b','linewidth',2);
+ % patchline, from the file exchange has this tendency to link the first
+ % and last points together, add in an extra element of "nans" to remove
+ % this
+alphaData = repmat(tailLength./[1:(tailLength+1)],1);
+alphaData = (alphaData(:));
+alphaData = alphaData.^(-2);
+tailHand = patchline([tailX;nan],[tailY;nan],'edgecolor','b','LineWidth',2,...
+    'EdgeAlpha','interp',...
+    'FaceVertexAlphaData',alphaData);
 
 % add in a cross at the fixed point
 plot(xFix,yFix,'kx','linewidth',2);
 
 % set plotting limits to remain the same
-xlim([0,3]);
-ylim([0,3]);
+xlim(xlimits);
+ylim(ylimits);
 
 % loop over a few frames
 while counter < 300
@@ -58,7 +64,7 @@ while counter < 300
     % update the tail coordinates
     tailX = [tailX(2:end);xNew];
     tailY = [tailY(2:end);yNew];
-    set(tailHand,'xData',tailX,'yData',tailY);
+    set(tailHand,'xData',[tailX;nan],'yData',[tailY;nan]);
     
     title(num2str(counter));
     
