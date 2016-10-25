11e0f18cba62e08009f043642b4e0b12009319ce
bpbratton <bratton@princeton.edu>
2016-10-25 07:54:00 -0500

    Add forward Euler integration

--- modularVisualizationExample.m
+++ modularVisualizationExample.m
@@ -11,6 +11,16 @@ ydot = @(x,y) -y*x^2 + b - a*y;
 xdot = @(x,y) y*x^2 - x + a*y;
 
 % proceed in one time step using Euler integration
-xNew = x0 + dt*xdot(x0,y0);
-yNew = y0 + dt*ydot(x0,y0);
-
+counter  = 1;
+x0 = 2;
+y0 = 1;
+xNew = x0;
+yNew = y0;
+% loop over a few frames
+while counter < 10
+    xNew = xNew + dt*xdot(xNew,yNew);
+    yNew = yNew + dt*ydot(xNew,yNew);
+    plot(xNew,yNew,'rx');
+    figure(gcf);pause(0.1);
+    counter = counter+1;
+end
