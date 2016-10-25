da1ab35c82a17bb949c5e1294e78c1fb279813c2
bpbratton <bratton@princeton.edu>
2016-10-25 08:06:55 -0500

    Allows spots to appear and disappear with an exp distr in lifetimes

--- modularVisualizationExample.m
+++ modularVisualizationExample.m
@@ -3,14 +3,14 @@ b = 0.6;
 a = 0.08;
 dt = 0.03;
 pauseTime = 0.00001;
-tailLength = 5;
+tailLength = 10;
 xlimits = [-0.6,0.6];
 ylimits = [-2,1];
 flowFieldSpacing = 0.03; % spacing of grid to build flow field on
 flowFieldDecayLength = 10.0; % as one moves away from the fixed point, may the quiver points less visually important
 howCloseToFixedPoint = 1.5; % standard deviation in starting position
-nSpots = 5;
-T = 100;
+nSpots = 15;
+T = 25;
 
 %% calculate the dynamics for a system that starts at an arbitrary position
 % x0,y0
@@ -25,9 +25,6 @@ counter  = 1;
 % flow field
 [xx,yy] = meshgrid(xlimits(1):flowFieldSpacing:xlimits(2),ylimits(1):flowFieldSpacing:ylimits(2));
 
-[theta,r] = cart2pol(xx-xFix,yy-yFix);
-xx(r>flowFieldDecayLength) = nan;
-yy(r>flowFieldDecayLength) = nan;
 
 % start with a blank graph
 clf;
@@ -36,18 +33,17 @@ hold on;
 % plot flow field as quiver
 quiver(xx(:),yy(:),xdot(xx(:),yy(:)),ydot(xx(:),yy(:)),3);
 
-% save plotting handle to improve speed and smoothness
-pHand = plot(xNew,yNew,'r*','linewidth',2,'markersize',10);
- % patchline, from the file exchange has this tendency to link the first
- % and last points together, add in an extra element of "nans" to remove
- % this
+
+% patchline, from the file exchange has this tendency to link the first
+% and last points together, add in an extra element of "nans" to remove
+% this
 alphaData = repmat(tailLength./[1:(tailLength+1)],1);
 alphaData = (alphaData(:));
 alphaData = alphaData.^(-2);
 
 % add in a cross at the fixed point
-plot(xFix,yFix,'kx','linewidth',2);
-plot(xFix,0,'ks','linewidth',2);
+plot(0,-1,'kx','linewidth',2);
+plot(0,0,'ks','linewidth',2);
 
 
 
@@ -56,23 +52,43 @@ plot(xFix,0,'ks','linewidth',2);
 xlim(xlimits);
 ylim(ylimits);
 
+% start with spots at nan
+xNew = nan(1,nSpots);
+yNew = nan(1,nSpots);
+
 for kkSpot = 1:nSpots
-% keep dropping new spots in
-xNew(1,kkSpot) = rand(1)*diff(xlimits)+xlimits(1);
-yNew(1,kkSpot) = rand(1)*diff(ylimits)+ylimits(1);
+    spotDiesAt(kkSpot) = random('exp',T,1)+1;
+    % setup vectors to hold position of "tail"
+    tailX(:,kkSpot) = repmat(xNew(1,kkSpot),tailLength,1);
+    tailY(:,kkSpot) = repmat(yNew(1,kkSpot),tailLength,1);
+    
+    
+    tailHand(kkSpot) = patchline([tailX(:,kkSpot);nan],[tailY(:,kkSpot);nan],'edgecolor','b','LineWidth',2,...
+        'EdgeAlpha','interp',...
+        'FaceVertexAlphaData',alphaData);
 end
-% setup vectors to hold position of "tail"
-tailX = repmat(xNew,tailLength,1);
-tailY = repmat(yNew,tailLength,1);
-for kkSpot = 1:nSpots
-tailHand(kkSpot) = patchline([tailX(:,kkSpot);nan],[tailY(:,kkSpot);nan],'edgecolor','b','LineWidth',2,...
-    'EdgeAlpha','interp',...
-    'FaceVertexAlphaData',alphaData);
-end
-%
+pHand = plot(xNew,yNew,'r*','linewidth',2,'markersize',10);
+
+
 % loop over a few frames
 ticA = tic;
-while toc(ticA) < 3
+while toc(ticA) < 30
+    % check and see if any spots need to be re-placed
+    for kkSpot = 1:nSpots
+        if counter>spotDiesAt(kkSpot)
+            %% add a new spot, remove its tail and calculate a new lifetime
+            spotDiesAt(kkSpot) = random('exp',T,1)+spotDiesAt(kkSpot);
+            
+            xNew(1,kkSpot) = rand(1)*diff(xlimits)+xlimits(1);
+            yNew(1,kkSpot) = rand(1)*diff(ylimits)+ylimits(1);
+            
+            % setup vectors to hold position of "tail"
+            tailX(:,kkSpot) = repmat(xNew(1,kkSpot),tailLength,1);
+            tailY(:,kkSpot) = repmat(yNew(1,kkSpot),tailLength,1);
+        end
+    end
+    
+    
     
     % calculate the new position
     xNew = xNew + dt*xdot(xNew,yNew);
@@ -83,12 +99,12 @@ while toc(ticA) < 3
     figure(gcf);pause(pauseTime);
     
     for kkSpot = 1:nSpots
-
-    % update the tail coordinates
-    tailX(:,kkSpot) = [tailX(2:end,kkSpot);xNew(kkSpot)];
-    tailY(:,kkSpot) = [tailY(2:end,kkSpot);yNew(kkSpot)];
-    set(tailHand,'xData',[tailX(:,kkSpot);nan],...
-        'yData',[tailY(:,kkSpot);nan]);
+        
+        % update the tail coordinates
+        tailX(:,kkSpot) = [tailX(2:end,kkSpot);xNew(kkSpot)];
+        tailY(:,kkSpot) = [tailY(2:end,kkSpot);yNew(kkSpot)];
+        set(tailHand(kkSpot),'xData',[tailX(:,kkSpot);nan],...
+            'yData',[tailY(:,kkSpot);nan]);
     end
     title(num2str(counter));
     
