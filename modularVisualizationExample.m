6d728ec6a697b4b01ddade9ad66ef41ce5ff173e
bpbratton <bratton@princeton.edu>
2016-10-25 08:05:16 -0500

    Switch to a new set of differential equations
    These have new ranges, new fixed points, etc.

--- modularVisualizationExample.m
+++ modularVisualizationExample.m
@@ -1,26 +1,29 @@
 %% initialize parameters
 b = 0.6;
 a = 0.08;
-dt = 0.1;
+dt = 0.03;
 pauseTime = 0.00001;
-tailLength = 50;
-xlimits = [0,3];
-ylimits = [0,3];
+tailLength = 5;
+xlimits = [-2,2];
+ylimits = [-2,2];
 flowFieldSpacing = 0.1; % spacing of grid to build flow field on
-flowFieldDecayLength = 1.5; % as one moves away from the fixed point, may the quiver points less visually important
-howCloseToFixedPoint = 0.3; % standard deviation in starting position
+flowFieldDecayLength = 10.0; % as one moves away from the fixed point, may the quiver points less visually important
+howCloseToFixedPoint = 1.5; % standard deviation in starting position
 %% calculate the dynamics for a system that starts at an arbitrary position
 % x0,y0
 
 % setup anonymous functions to calculate the derivatives
-ydot = @(x,y) -y.*x.^2 + b - a*y;
-xdot = @(x,y) y.*x.^2 - x + a*y;
+ydot = @(x,y) x.^2 + y.^2 + x + y;
+xdot = @(x,y) -3*x;
 
 % proceed in one time step using Euler integration
 counter  = 1;
 
-xFix = b;
-yFix = b/(b+a^2);
+% xFix = b;
+% yFix = b/(b+a^2);
+
+yFix = -1;
+xFix = 0;
 
 x0 = xFix+randn(1)*howCloseToFixedPoint;
 y0 = yFix+randn(1)*howCloseToFixedPoint;
@@ -44,7 +47,7 @@ clf;
 hold on;
 
 % plot flow field as quiver
-quiver(xx(:),yy(:),xdot(xx(:),yy(:)),ydot(xx(:),yy(:)),10);
+quiver(xx(:),yy(:),xdot(xx(:),yy(:)),ydot(xx(:),yy(:)),3);
 
 % save plotting handle to improve speed and smoothness
 pHand = plot(xNew,yNew,'r*','linewidth',2,'markersize',10);
@@ -60,13 +63,27 @@ tailHand = patchline([tailX;nan],[tailY;nan],'edgecolor','b','LineWidth',2,...
 
 % add in a cross at the fixed point
 plot(xFix,yFix,'kx','linewidth',2);
+plot(xFix,0,'ks','linewidth',2);
+
+
 
 % set plotting limits to remain the same
 xlim(xlimits);
 ylim(ylimits);
 
+% keep dropping new spots in
+x0 = xFix+randn(1)*howCloseToFixedPoint;
+y0 = yFix+randn(1)*howCloseToFixedPoint;
+xNew = x0;
+yNew = y0;
+
+
+% setup vectors to hold position of "tail"
+tailX = repmat(x0,tailLength,1);
+tailY = repmat(y0,tailLength,1);
+
 % loop over a few frames
-while counter < 300
+while counter < 100
     % calculate the new position
     xNew = xNew + dt*xdot(xNew,yNew);
     yNew = yNew + dt*ydot(xNew,yNew);
